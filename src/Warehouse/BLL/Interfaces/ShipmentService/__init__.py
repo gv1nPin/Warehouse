1dfa22139010ecacb6be376a59873e35771ee67d
Hew_Maan <gvinpin.93@gmail.com>
2026-09-09 21:31:42 +0300

    feat(warehouse): Setup BLL ShipmentService interfaces package

---
+++ src/Warehouse/BLL/Interfaces/ShipmentService/__init__.py
@@ -0,0 +1,9 @@
+from .AbstractShipmentDispatchService import AbstractShipmentDispatchService
+from .AbstractShipmentReceiptService import AbstractShipmentReceiptService
+from .AbstractShipmentTransitCoordinator import AbstractShipmentTransitCoordinator
+
+__all__ = [
+    "AbstractShipmentDispatchService",
+    "AbstractShipmentReceiptService",
+    "AbstractShipmentTransitCoordinator",
+]
