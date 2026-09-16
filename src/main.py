31bce3b7403503b2e47ab6a9c80431932971bf22
Hew_Maan <gvinpin.93@gmail.com>
2026-09-16 13:09:32 +0300

    add logger

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from dependency_injector.wiring import Provide, inject
 from container import Container
 from Warehouse.BLL.Services.AuthService.AuthService import AuthService
+from Warehouse.BLL.Common.Logger import setup_logging
 
 @inject
 def main(auth_service: AuthService = Provide[Container.auth_service]):
@@ -12,9 +13,12 @@ def main(auth_service: AuthService = Provide[Container.auth_service]):
     print(f" Доступные методы сервиса: {dir(auth_service)}")
 
 if __name__ == "__main__":
-    # 1. Создаем экземпляр контейнера
+    # 1. Инициализируем логи СРАЗУ ЖЕ при старте программы
+    setup_logging()
+
+    # 2. Создаем экземпляр контейнера
     container = Container()
-    
+       
     # связываем контейнер с текущим файлом main.py
     # Это заставит декоратор @inject корректно подставлять зависимости
     container.wire(modules=[__name__])
