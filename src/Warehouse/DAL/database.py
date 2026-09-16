ac236034c9a3b88692a3b4a4762587f95ff0a40c
Hew_Maan <gvinpin.93@gmail.com>
2026-09-16 18:06:38 +0300

    add context manager

--- src/Warehouse/DAL/database.py
+++ src/Warehouse/DAL/database.py
@@ -1,9 +1,8 @@
 import os
-from sqlalchemy import create_engine
-
+from contextlib import contextmanager
 from dotenv import load_dotenv
 from sqlalchemy import URL, create_engine
-from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
 load_dotenv()
 
@@ -15,9 +14,24 @@ DATABASE_URL = URL.create(
     port=int(os.getenv("DB_PORT", "5432")),
     database=os.getenv("DB_NAME", "Warehouse"),
 )
+
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True
 )
 
 session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Наш контекстный менеджер
+@contextmanager
+def get_session():
+    """Контекстный менеджер для безопасной работы с сессией SQLAlchemy."""
+    session: Session = session_factory()
+    try:
+        yield session
+        session.commit()  # Автоматически делаем коммит, если всё прошло успешно
+    except Exception:
+        session.rollback()  # Откатываем транзакцию при любой ошибке
+        raise  # Пробрасываем ошибку дальше для логирования или обработки в BLL
+    finally:
+        session.close()  # Гарантированно закрываем сессию и возвращаем коннект в пул
