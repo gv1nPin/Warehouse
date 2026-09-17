ef6e38c5914013d3ee0f72e1a3383d83284a8b6a
Hew_Maan <gvinpin.93@gmail.com>
2026-09-17 09:28:47 +0300

    ref, add basic class

--- src/Warehouse/Common/exceptions.py
+++ src/Warehouse/Common/exceptions.py
@@ -9,40 +9,39 @@ class BusinessError(Exception):
 
 class ValidationError(BusinessError):
     """Неверные данные в запросе (например, не пройдена валидация полей)."""
-    status_code: int = 400
+    status_code = 400
 
     def __init__(self, message: str = "Переданы неверные данные"):
-            super().__init__(message)
+        super().__init__(message)
 
 
 class AuthError(BusinessError):
     """Ошибка аутентификации (неверный логин, пароль или токен)."""
-    status_code: int = 401
+    status_code = 401
 
     def __init__(self, message: str = "Неверный логин, пароль или токен"):
-            super().__init__(message)
+        super().__init__(message)
 
 
 class AccessDeniedError(BusinessError):
     """Ошибка авторизации (нет прав доступа или попытка обратиться к чужому складу)."""
-    status_code: int = 403
+    status_code = 403
 
     def __init__(self, message: str = "Доступ запрещен или у вас нет прав на этот склад"):
-            super().__init__(message)
+        super().__init__(message)
 
 
 class NotFoundError(BusinessError):
     """Запрашиваемая запись не найдена в базе данных или была удалена."""
-    status_code: int = 404
+    status_code = 404
 
     def __init__(self, message: str = "Запись не найдена или удалена"):
-            super().__init__(message)
+        super().__init__(message)
 
 
 class InvalidStatusError(BusinessError):
     """Действие запрещено для текущего статуса сущности."""
-    status_code: int = 409  
+    status_code = 409  
 
     def __init__(self, message: str = "Запрошенное действие запрещено в текущем статусе"):
-            super().__init__(message)
-                                
+        super().__init__(message)
