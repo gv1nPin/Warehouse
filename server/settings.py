8d8c6a7f865f64ba77574552818d783f6b7d9521
Hew_Maan <gvinpin.93@gmail.com>
2026-09-24 18:16:07 +0300

    регистрация в instaled apps и middleware

--- server/settings.py
+++ server/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'warehouse.apps.WarehouseConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'Warehouse.src.warehouse.common.middleware',
 ]
 
 ROOT_URLCONF = 'server.urls'
